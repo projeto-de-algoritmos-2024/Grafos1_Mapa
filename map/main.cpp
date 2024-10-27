143def7c6eb9c40f2fbee9eed37a21681ddbb0d4
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-10-26 21:50:54 -0300

    Random node position and refresh button

--- map/main.cpp
+++ map/main.cpp
@@ -1,13 +1,16 @@
 #include <vector>
 #include <wx/wx.h>
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 void addEdge(vector<int> adj[], int u, int v)
 {
     // Verificação de limites
-    if (u < 6 && v < 6 && u >= 0 && v >= 0) {
+    if (u < 6 && v < 6 && u >= 0 && v >= 0)
+    {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
@@ -16,42 +19,82 @@ void addEdge(vector<int> adj[], int u, int v)
 class GraphPanel : public wxPanel
 {
 public:
-    GraphPanel(wxWindow *parent, vector<int> adj[], int n) : wxPanel(parent), n(n)
+    GraphPanel(wxWindow *parent, vector<int> adj[], int n) : wxPanel(parent), n(n), node_colors(n, *wxBLUE), node_positions(n)
     {
-        // Criar uma cópia do vetor de adjacência
+        // Cria uma cópia do vetor de adjacência
         this->adj = new vector<int>[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             this->adj[i] = adj[i]; // Cópia dos dados
         }
 
+        InitializeNodePositions();
+
         Bind(wxEVT_PAINT, &GraphPanel::OnPaint, this);
+        Bind(wxEVT_LEFT_DOWN, &GraphPanel::OnClick, this);
     }
 
-    ~GraphPanel() {
-        delete[] adj; // Libere a memória
+    ~GraphPanel()
+    {
+        delete[] adj; // Libera a memória
+    }
+
+    void RecalculateNodePositions()
+    {
+        InitializeNodePositions();
+        Refresh();
     }
 
 private:
-    vector<int>* adj;
+    vector<int> *adj;
     int n;
+    vector<wxColor> node_colors;
+    vector<wxPoint> node_positions;
+
+    void InitializeNodePositions()
+    {
+        int minX = 0;
+        int maxX = 400;
+        int minY = 0;
+        int maxY = 400;
+        int min_distance = 40;
+
+        // Inicializa a semente do rand() com o tempo atual
+        srand(time(0));
+
+        for (int i = 0; i < n; i++)
+        {
+            bool valid_position;
+
+            do
+            {
+                valid_position = true;
+                int x = minX + rand() % (maxX - minX + 1);
+                int y = minY + rand() % (maxY - minY + 1);
+                wxPoint new_position(x, y);
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (sqrt(pow(new_position.x - node_positions[j].x, 2) + pow(new_position.y - node_positions[j].y, 2)) < min_distance)
+                    {
+                        valid_position = false;
+                        break;
+                    }
+                }
+
+                if (valid_position)
+                {
+                    node_positions[i] = new_position;
+                }
+            } while (!valid_position);
+        }
+    }
 
     void OnPaint(wxPaintEvent &event)
     {
         wxPaintDC dc(this);
 
-        vector<wxPoint> node_positions(n);
-        int radius = 100;
-        int centerX = 200;
-        int centerY = 200;
-
-        for (int i = 0; i < n; i++)
-        {
-            double angle = 2.0 * M_PI * i / n;
-            int x = centerX + radius * cos(angle);
-            int y = centerY + radius * sin(angle);
-            node_positions[i] = wxPoint(x, y);
-        }
-
+        // Desenha as arestas
         dc.SetPen(wxPen(*wxBLACK, 2));
         for (int i = 0; i < n; i++)
         {
@@ -61,11 +104,31 @@ private:
             }
         }
 
-        dc.SetBrush(*wxBLUE_BRUSH);
+        // Desenha os nós
         for (int i = 0; i < n; i++)
         {
-            dc.DrawCircle(node_positions[i], 10);
-            dc.DrawText(wxString::Format("%d", i), node_positions[i].x - 5, node_positions[i].y - 5);
+            dc.SetBrush(wxBrush(node_colors[i]));
+            dc.DrawCircle(node_positions[i], 16);
+            dc.DrawText(wxString::Format("%d", i), node_positions[i].x - 8, node_positions[i].y - 8);
+        }
+    }
+
+    void OnClick(wxMouseEvent &event)
+    {
+        wxPoint click_pos = event.GetPosition();
+        int node_radius = 16;
+
+        for (int i = 0; i < n; i++)
+        {
+            // Verifica se o clique está dentro do raio do nó
+            if (click_pos.x >= node_positions[i].x - node_radius && click_pos.x <= node_positions[i].x + node_radius &&
+                click_pos.y >= node_positions[i].y - node_radius && click_pos.y <= node_positions[i].y + node_radius)
+            {
+                // Altera a cor do nó clicado
+                node_colors[i] = (node_colors[i] == *wxBLUE) ? *wxRED : *wxBLUE;
+                Refresh(); // Redesenha o painel para aplicar a mudança de cor
+                break;
+            }
         }
     }
 };
@@ -74,9 +137,26 @@ class GraphFrame : public wxFrame
 {
 public:
     GraphFrame(const wxString &title, vector<int> adj[], int n)
-        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(400, 400))
+        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(600, 600))
     {
-        new GraphPanel(this, adj, n);
+        wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+        panel = new GraphPanel(this, adj, n);
+        sizer->Add(panel, 1, wxEXPAND);
+
+        wxButton *button = new wxButton(this, wxID_ANY, "Refresh");
+        sizer->Add(button, 0, wxALIGN_LEFT | wxALL, 10);
+
+        SetSizer(sizer);
+
+        button->Bind(wxEVT_BUTTON, &GraphFrame::OnRefresh, this);
+    }
+
+private:
+    GraphPanel *panel;
+
+    void OnRefresh(wxCommandEvent &event)
+    {
+        panel->RecalculateNodePositions();
     }
 };
 
