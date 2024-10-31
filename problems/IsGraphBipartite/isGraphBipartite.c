31407cad276d7a01341370c76fe6ae6e28570d7c
martinsglucas <martin5.luca5.univ@gmail.com>
2024-10-31 09:29:25 -0300

    altera forma como percorre o grafo e executa bfs para cada nó não visitado
    o grafo é passado como [[1,3],[0,2]] e não [[0,1,0,1],[1,0,1,0]]

--- problems/IsGraphBipartite/isGraphBipartite.c
+++ problems/IsGraphBipartite/isGraphBipartite.c
@@ -5,75 +5,82 @@
 typedef struct no {
     int dado;
     struct no *prox;
-} No;
+} no;
 typedef struct {
-    int v;
+    int visitado;
     int camada;
 } vNo;
-No *cria_fila(){
-    No *fila = (No *) malloc(sizeof(No));
+no *cria_fila(){
+    no *fila = (no *) malloc(sizeof(no));
     if (fila != NULL){
         fila->dado = 0;
         fila->prox = NULL;
     }
     return fila;
 }
-void enfileira(No *fila, int dado){
-    No *novo = (No *) malloc(sizeof(No));
+void enfileira(no *fila, int dado){
+    no *novo = (no *) malloc(sizeof(no));
     if (novo != NULL){
         novo->dado = dado;
         novo->prox = NULL;
-        No *temp = fila;
+        no *temp = fila;
         while (temp->prox != NULL) {
             temp = temp->prox;
         }
         temp->prox = novo;
     }
 }
-No *desenfileira(No *fila){
-    if (fila->prox == NULL) {
-        return NULL;
-    }
-    No *no = fila->prox;
+int desenfileira(no *fila){
+    assert(!fila_vazia(fila));
+    no *no = fila->prox;
     fila->prox = no->prox;
-    return no;
+    int valor = no->dado;
+    free(no);
+    return valor;
 }
-int fila_vazia(No *fila){
+int fila_vazia(no *fila){
     return (fila->prox == NULL);
 }
-int bfs(int **graph, int graphSize, No *fila, vNo *visited, int v){
+int bfs(int **graph, int graphSize, int *graphColSize, no *fila, vNo *visited, int v){
     int camada = 0;
-    visited[v].v = 1;
+    visited[v].visitado = 1;
     visited[v].camada = camada;
     enfileira(fila, v);
     while(!fila_vazia(fila)){
-        No *w = desenfileira(fila);
+        v = desenfileira(fila);
         camada++;
-        v = w->dado;
         int vCamada = visited[v].camada;
-        for (int i=0; i < graphSize; i++){
-            if (graph[v][i] && visited[i].v == 0){
-                visited[i].v = 1;
-                visited[i].camada = camada;
-                enfileira(fila, i);
+        for (int i=0; i < graphColSize[v]; i++){
+            int vizinho = graph[v][i];
+            if (visited[vizinho].visitado == 0){
+                visited[vizinho].visitado = 1;
+                visited[vizinho].camada = camada;
+                enfileira(fila, vizinho);
             }
-            else if (graph[v][i] && visited[i].v == 1){
-                if (vCamada == visited[i].camada) return 0;
+            else if (visited[vizinho].visitado == 1){
+                if (vCamada == visited[vizinho].camada) return 0;
             }
         }
-        free(w);
     }
     return 1;
 }
 bool isBipartite(int** graph, int graphSize, int* graphColSize) {
     vNo *visited = (vNo *) malloc(graphSize * sizeof(vNo));
     for (int i=0; i < graphSize; i++){
-        visited[i].v = 0;
+        visited[i].visitado = 0;
         visited[i].camada = -1;
     }
-    No *fila = cria_fila();
-    int result = bfs(graph, graphSize, fila, visited, 0);
+    no *fila = cria_fila();
+    for (int i=0; i<graphSize; i++){
+        if (!visited[i].visitado){
+            if (!bfs(graph, graphSize, graphColSize, fila, visited, i)){
+                free(fila);
+                free(visited);
+                return false;
+            }
+        }
+    }
     free(fila);
     free(visited);
-    return result == 1 ? true : false;
+    return true;
 }
