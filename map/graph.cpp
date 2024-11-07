e9a9a40b3fd3f93c907b3d169b2261b7641ffe94
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-11-07 17:11:24 -0300

    adiciona funções de ordenação topológica

--- map/graph.cpp
+++ map/graph.cpp
@@ -1,4 +1,7 @@
 #include "graph.h"
+#include <vector>
+#include <algorithm>
+#include <queue>
 
 Graph::Graph(int n) : n(n), adj(n)
 {
@@ -21,3 +24,55 @@ int Graph::getSize() const
 {
     return n;
 }
+
+void Graph::dfs(int node, std::vector<bool> &visited, std::vector<int> &ans, const std::vector<std::vector<int>> &adj) const
+{
+    visited[node] = true;
+    for (int neighbor : adj[node])
+    {
+        if (!visited[neighbor])
+            dfs(neighbor, visited, ans, adj);
+    }
+    ans.push_back(node);
+}
+
+std::vector<int> Graph::topologicalSort(int n, std::vector<std::vector<int>> &adj) const
+{
+    std::vector<bool> visited(n, false);
+    std::vector<int> ans;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (!visited[i])
+            dfs(i, visited, ans, adj);
+    }
+
+    std::reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+std::vector<int> Graph::kahn(int n, std::vector<std::vector<int>> &adj) const
+{
+    std::vector<int> inDegree(n, 0); // Grau de entrada, ou seja, número de arestas que chegam no vértice
+
+    for (int i = 0; i < n; ++i)
+        for (int neighbor : adj[i]) inDegree[neighbor]++; // Incrementa o grau de entrada do vizinho
+
+    std::queue<int> q;
+
+    for (int i = 0; i < n; ++i) 
+        if (inDegree[i] == 0) q.push(i); // Se o grau de entrada for 0 significa que o vértice pode ser imediatamente processado
+
+    std::vector<int> ans;
+
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        ans.push_back(node);
+
+        for (int neighbor : adj[node]) {
+            inDegree[neighbor]--;
+            if (inDegree[neighbor] == 0) q.push(neighbor);
+        }
+    }
+
+}   
